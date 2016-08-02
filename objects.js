9defe558a53b7a0db9921da0f49d40a97a95bde6
Miguel López <miguelopezv@gmail.com>
2016-08-01 23:01:28 -0500

    New basic files

---
+++ objects.js
@@ -0,0 +1,12 @@
+var myComputer = {
+  operatingSystem: 'OSX',
+  screenSize: '13 inches',
+  purchaseDate: 2011
+}
+
+var miguel = {
+  name: "Miguel",
+  sayName: function() {
+    console.log(this.name);
+  }
+}
