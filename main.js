f98cc18b46dd2d8c76555ca3522b0aec536d8fd6
Miguel López <miguelopezv@gmail.com>
2016-08-03 23:12:57 -0500

    Add functions to object

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-
+// Object
 var todoList = {
-  todos: ['item1', 'item2', 'item3', 'item4']
+  // property with saves the todos
+  todos: ['item1', 'item2', 'item3', 'item4'],
+  // Method to display all the todos
+  displayTodos: function() {
+    console.log('My todos', this.todos);
+  },
+  // Method to add todos
+  addTodos: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  // Method to edit todos
+  changeTodo: function(position, newTodo) {
+    this.todos[(position - 1)] = newTodo;
+    this.displayTodos();
+  },
+  // Method to delete todos
+  deleteTodo: function(position) {
+    this.todos.splice((position -1), 1);
+    this.displayTodos();
+  }
 };
