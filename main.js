1fd93d4b7f75112d967d3b1eda46d17b4cb4eeb6
Miguel López <miguelopezv@gmail.com>
2016-08-04 23:46:25 -0500

    Add New methods, new toDos are now objects

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 // Object
 var todoList = {
-  // property with saves the todos
-  todos: ['item1', 'item2', 'item3', 'item4'],
+  // property with saves the todos in an array
+  todos: [],
   // Method to display all the todos
   displayTodos: function() {
     console.log('My todos', this.todos);
   },
-  // Method to add todos
-  addTodos: function(todo) {
-    this.todos.push(todo);
+  // Method to create an object
+  addTodos: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
     this.displayTodos();
   },
   // Method to edit todos
-  changeTodo: function(position, newTodo) {
-    this.todos[(position - 1)] = newTodo;
+  changeTodo: function(position, todoText) {
+    this.todos[position - 1].todoText = todoText;
     this.displayTodos();
   },
   // Method to delete todos
   deleteTodo: function(position) {
-    this.todos.splice((position -1), 1);
+    this.todos.splice(position -1, 1);
+    this.displayTodos();
+  },
+  // Method to toggle 'completed' property
+  toggleCompleted: function(position) {
+    var todo = this.todos[position - 1];
+    todo.completed = !todo.completed;
     this.displayTodos();
   }
-  
 };
