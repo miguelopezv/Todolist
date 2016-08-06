1ee25155a0856dc5f170f836fd595d3f14a76751
Miguel López <miguelopezv@gmail.com>
2016-08-06 15:28:40 -0500

    Js Beautify - JsHint

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 var todoList = {
   // property with saves the todos in an array
   todos: [],
+
   // Method to display all the todos
   displayTodos: function() {
     if (this.todos.length === 0) {
@@ -17,6 +18,7 @@ var todoList = {
       }
     }
   },
+
   // Method to create a todo object
   addTodos: function(todoText) {
     this.todos.push({
@@ -25,22 +27,26 @@ var todoList = {
     });
     this.displayTodos();
   },
+
   // Method to edit todos
   changeTodo: function(position, todoText) {
     this.todos[position - 1].todoText = todoText;
     this.displayTodos();
   },
+
   // Method to delete todos
   deleteTodo: function(position) {
     this.todos.splice(position - 1, 1);
     this.displayTodos();
   },
+
   // Method to toggle 'completed' property
   toggleCompleted: function(position) {
     var todo = this.todos[position - 1];
     todo.completed = !todo.completed;
     this.displayTodos();
   },
+
   // Method to toggle all todos
   toggleAll: function() {
     var completedTodos = 0,
