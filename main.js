1f9da19a1b94ef769171d33210c29f594a03c4a1
Miguel López <miguelopezv@gmail.com>
2016-08-08 22:29:24 -0500

    First UI attempt

--- main.js
+++ main.js
@@ -72,3 +72,15 @@ var todoList = {
     this.displayTodos();
   }
 };
+
+// Event Listeners for buttons
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll();
+});
